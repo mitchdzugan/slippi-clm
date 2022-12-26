28f0fcbbc4660b09b96badfed9a88e7fd6ad5f0a
mitchdzugan <mitchdzugan@gmail.com>
2022-12-26 01:21:58 -0600

    progress

--- update.js
+++ update.js
@@ -1,7 +1,6 @@
 const { request, gql } = require('graphql-request');
 const fs = require('fs/promises');
-const codes_ = require('./codes.json');
-const codes = codes_.map(cc => cc.toUpperCase());
+const codes = require('./codes.json');
 const query = gql`
 query AccountManagementPageQuery($cc: String!) {
   getConnectCode(code: $cc) {
@@ -31,7 +30,7 @@ query AccountManagementPageQuery($cc: String!) {
 `;
 const endpoint = 'https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql';
 const getPlayerData = async (cc) => {
-  const variables = { cc: cc };
+  const variables = { cc: cc.toUpperCase() };
   const data = await request(endpoint, query, variables);
   return data;
 }
@@ -39,11 +38,11 @@ const timeout = async ms => new Promise((resolve) => setTimeout(resolve, ms));
 const getUnixTimestamp = () => Math.floor(Date.now() / 1000);
 const main = async () => {
   const data = {};
-  for (const code of codes) {
+  for (const [code, tag] of codes) {
     const playerData = await getPlayerData(code);
     console.log(code);
     await timeout(5000);
-    data[code] = playerData;
+    data[code.toUpperCase()] = { ...playerData, tag };
   }
   const updatedAt = getUnixTimestamp();
   const json = { data, updatedAt };
